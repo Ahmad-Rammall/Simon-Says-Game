e81893a6e206c0d05261d12dcdce26022afb1d4d
Your Name <your_email@example.com>
2023-11-11 19:47:57 +0200

    Last Commit

--- game.js
+++ game.js
@@ -1,12 +1,11 @@
-const body = document.querySelector("body");
 const title = document.querySelector("#level-title");
 
 const colorsArray = ["green", "red", "yellow", "blue"];
-let clickedColors = [];
-let sequence = [];
+let clickedColors , sequence= [];
 let level = 0;
 let gameStarted = false;
 
+// When User Clicks On The Keyboard To Start The Game
 document.addEventListener("keypress", () => {
   if (!gameStarted) {
     gameStarted = true;
@@ -14,40 +13,46 @@ document.addEventListener("keypress", () => {
   }
 });
 
+// To Add A New Step In The Sequence
 function generateNextLevel() {
   level++;
   title.textContent = `Level ${level}`;
   clickedColors = [];
   let random = Math.floor(Math.random() * 4);
-  sequence.push(colorsArray[random]);
+  
+  // Adding Random Color
+  sequence.push(colorsArray[random]); 
+
   let btn = document.getElementById(colorsArray[random]);
   setTimeout(() => {
     changeClassList(btn, "pressed");
     playSound(btn.id);
-  }, 1000);
+  }, 1000); // Delay The Execution after 1 second
 }
 
+// To Play A Sound
 function playSound(soundName) {
   const sound = new Audio(`./sounds/${soundName}.mp3`);
   sound.play();
 }
 
+// To Add And Remove A Class (.pressed / game-over)
 function changeClassList(element, className) {
   element.classList.add(className);
   setTimeout(() => element.classList.remove(className), 150);
 }
 
+// Event Listener To Track The User's Click
 document.querySelectorAll(".btn").forEach((btn) => {
   btn.addEventListener("click", () => {
     if (!gameStarted) return;
-    changeClassList(btn, "pressed");
+    changeClassList(btn, "pressed"); // To Add The Click Animation To The Button 
     clickedColors.push(btn.id);
-    console.log("sequence:" + sequence);
-    console.log("user: " + clickedColors);
     checkAnswer(clickedColors.length - 1);
   });
 });
 
+// To Check The User's Choice
 function checkAnswer(index) {
   if (sequence[index] == clickedColors[index]) {
     playSound(clickedColors[index]);
@@ -55,13 +60,14 @@ function checkAnswer(index) {
       generateNextLevel();
     }
   } else {
-    changeClassList(body, "game-over");
+    changeClassList(document.querySelector("body"), "game-over");
     playSound("wrong");
-    title.textContent = "Game Over! Press a Key To Restart.";
+    title.textContent = "Game Over! Press Any Key To Restart.";
     resetGame();
   }
 }
 
+// To Reset The Game
 function resetGame() {
   level = 0;
   sequence = [];
